0a2001e445356ac17bd842ee222aff16ec4b807f
Sanskar Choubey <61118175+Sanskar-Choubey@users.noreply.github.com>
2021-01-06 15:46:14 +0530

    initial commit

---
+++ apps.js
@@ -0,0 +1,20 @@
+function playSound(event) {
+     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+     if (!audio) return; // this will end the function execution
+
+     audio.currentTime = 0; //this is rewind the key to the start
+     audio.play();
+
+     key.classList.add('playing');
+};
+
+function removeTransition(event){
+   if(event.propertyName !== 'transform') return; //this will skip if its not transform;
+
+   this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound);
